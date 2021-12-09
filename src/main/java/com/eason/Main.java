daf002111e8dadfc161623e305c29cb3f97b26e5
hyc <huangyicheng@supermonkey.com.cn>
2021-12-09 16:00:39 +0800

    init demo project

---
+++ src/main/java/com/eason/Main.java
@@ -0,0 +1,12 @@
+package com.eason;
+
+/**
+ * @author hyc
+ * @since 2021/12/9
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
